2ad824d00f6391acad7aba2a6848743bdc70ddba
Roshan <roshanpmathew@netstratum.com>
2026-05-21 10:43:56 +0530

    style(bg-image) : selected Bg image

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 
 import './App.css'
+import { Input } from './components/ui/input'
+import { Button } from './components/ui/button'
 
 function App() {
 
   return (
     <>
       <h1>FireFox</h1>
+      <Input/>
+      <Button 
+      size="lg" 
+      variant="default" 
+      value="Search" 
+    >
+      Search
+    </Button>
     </>
   )
 }
