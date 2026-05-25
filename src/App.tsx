ec44c5486cf324c9602cf94cb65aab2a9cb6dc78
Roshan <roshanpmathew@netstratum.com>
2026-05-25 11:56:42 +0530

    feat(news-grid): completed news structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,25 @@
 import './App.css'
 import Header from './ui-components/Header'
 import Shortcuts from './ui-components/Shortcuts'
+import NewsGrid from './ui-components/NewsGrid'
 
 
 function App() {
 
   return (
-    <div className=' flex flex-col align-middle justify-center'>
+    <div className='flex flex-col align-middle justify-center'>
       <div className='flex flex-col gap-10'>
 
       <Header/>
       <Shortcuts/>
 
       </div>
+      <div className='w-full px-15'>
+
+      <NewsGrid/>
+
+      </div>
+
      
      
     </div>
